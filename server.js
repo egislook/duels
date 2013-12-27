be3b460100ba2eb8d4390ff55685cb18af0efe28
zarna <zarna@vm-0.zarna.koding.kd.io>
2013-12-27 04:27:04 +0000

    server.js

--- server.js
+++ server.js
@@ -62,8 +62,8 @@ app.get('/scan/?(:page)?', function(req, res){
 	  			temp['img'] = $(this).find('img').attr("src");
 	  			temp['name'] = $(this).find('span.market_listing_item_name').text();
 	  			//temp['count'] = $(this).find('span.market_listing_num_listings_qty').text();
-	  			var price = ($(this).find('div.market_listing_num_listings').text()).split('$')[1];
-	  			temp['price'] = Math.floor((price * curency['$']) * 100) / 100;
+	  			temp['price'] = ($(this).find('div.market_listing_num_listings').text()).split('$')[1].replace(/(\r\n\t|\n|\r|\t)/gm,"");
+	  			//temp['price'] = Math.floor((price * curency['$']) * 100) / 100;
 	  			items.push(temp);
 	  			temp={};
 			});
@@ -75,6 +75,7 @@ app.get('/scan/?(:page)?', function(req, res){
 			res.render('scan', { items : newItems, page : req.params.page, limit : '10'});
 			//res.send(market);
 		}, req.params.page);
+		console.log('done');
 	}
 	else
 		res.redirect('/login');
