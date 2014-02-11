787dbaad0aa6a8a6891b3e4f71d44a797e86d535
zarna <zarna@vm-0.zarna.koding.kd.io>
2014-02-11 00:36:20 +0000

    rcf_9

--- routes/good.js
+++ routes/good.js
@@ -3,7 +3,7 @@ var tournaments_model = require('../models/tournaments_model.js');
 
 'use strict';
 module.exports = {
-    '/rate' : function(req, res){
+    /*'/rate' : function(req, res){
         tournaments_model.list(req, function(tournaments){
             tournaments_model.games(req, function(games){
                 user_model.stats(req, tournaments, games, function(data){
@@ -12,7 +12,7 @@ module.exports = {
             }, false, {'info.tournamentclass' : 'rcf'});
         }, false, 'ended');
         
-    },
+    },*/
     '/create': function(req, res){
         /*req.session.user = {
             'loged' : true,
